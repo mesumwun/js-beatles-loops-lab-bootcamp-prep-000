06d1e5b775906ee94f53a4d52720ef551735539d
Daniel Hope <upinhope@gmail.com>
2019-03-09 23:41:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var newArray = [];
+  
+  for (let i = 0; i < musicians.length; i++) {
+    newArray.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return newArray;
+}
+
+function johnLennonFacts(facts) {
+  let counter = 0;
+  while (counter < facts.length) {
+    facts[counter] += '!!!';
+    counter++;
+  }
+  return facts;
+}
+
+function iLov
